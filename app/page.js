c78e168075eb4e53a9da121ddd10d81b61bb70a8
Emmy-delight <165961039+Emmy-delight@users.noreply.github.com>
2026-01-30 12:58:15 +0100

    navbar,footer,signup components

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
+ import Image from "next/image";
+ 
  export default function HomePage () {
     return (
-       <main>
-         <div>
-          <p className="text-4xl text-blue-500">Welcome to my home page</p>
-           
-         </div>
+       <main className="min-h-screen">
+           <div className="bg-[url(/myimage.png)] h-screen bg-contain bg-no-repeat md:bg-[url(/myimage.png)] md:h-[60vh] md:bg-contain lg:bg-[url(/myimage.png)] lg:h-screen lg:bg-no-repeat lg:bg-cover">
+             
+           </div>
        </main>
     )
  }
---
+++ components/Footer.js
@@ -0,0 +1,18 @@
+export function Footer () {
+    const year = new Date().getFullYear()
+
+     return (
+        <footer className="bg-white px-5 py-5 block inset-shadow-sm  md:flex md:justify-between ">
+            <div className="flex gap-2 text-gray-600">
+                <p>&copy; {year} EdgeStack System </p>
+                <p>All rights reserved</p>
+            </div>
+            <div className="flex gap-2 text-gray-600">
+                <p>Privacy Policy</p>
+                <p>Terms of service</p>
+                <p>Support</p>
+            </div>
+     
+        </footer>
+     )
+}
